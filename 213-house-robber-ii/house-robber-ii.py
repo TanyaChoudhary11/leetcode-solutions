69a7eb05086c8b02d79f853d2145c9e458365374
Tanya Choudhary <tanyachoudhary911@gmail.com>
2026-06-20 16:27:42 +0530

    Time: 3 ms (12.99%) | Memory: 12.4 MB (58.76%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rob(self, nums):
+        if len(nums) == 1:
+            return nums[0]
+        
+        def robLine(houses):
+            prev2 = 0
+            prev1 = 0
+            
+            for money in houses:
+                curr = max(prev1, prev2 + money)
+                prev2 = prev1
+                prev1 = curr
+            
+            return prev1
+        
+        case1 = robLine(nums[:-1])   
+        case2 = robLine(nums[1:])   
+        
+        return max(case1, case2)
